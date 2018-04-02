fd970574c71f5f8c412997c44ac7a9d8da0afa93
austinmishoe <amishoe82@gmail.com>
2018-04-01 20:40:41 -0400

    added overall into method

--- PullDataMethods.R
+++ PullDataMethods.R
@@ -9,20 +9,24 @@ pull_data_2017_1H<-function(star=TRUE,score=FALSE){
     dataStars_df_1<<-read.csv("Data/1H/data_star_raw_1H2017.csv",header = T)
     smsStars_df_1<<-read.csv("Data/1H/sms_star_raw_1H2017.csv",header = T)
     speedStars_df_1<<-read.csv("Data/1H/speed_star_raw_1H2017.csv",header = T)
+    overallStars_df_1 <<-read.csv("Data/1H/overall_star_raw_1H2017.csv",header = T)
     callStarsRank_df_1<<-read.csv("Data/1H/call_star_rank_1H2017.csv",header = T)
     dataStarsRank_df_1<<-read.csv("Data/1H/data_star_rank_1H2017.csv",header = T)
     smsStarsRank_df_1<<-read.csv("Data/1H/sms_star_rank_1H2017.csv",header = T)
     speedStarsRank_df_1<<-read.csv("Data/1H/speed_star_rank_1H2017.csv",header = T)
+    overallStarsRank_df_1 <<-read.csv("Data/1H/overall_star_rank_1H2017.csv",header = T)
   }
   if(score==TRUE){
     callScores_df_1<<-read.csv("Data/1H/call_score_raw_1H2017.csv",header = T)
     dataScores_df_1<<-read.csv("Data/1H/data_score_raw_1H2017.csv",header = T)
     smsScores_df_1<<-read.csv("Data/1H/sms_score_raw_1H2017.csv",header = T)
     speedScores_df_1<<-read.csv("Data/1H/speed_score_raw_1H2017.csv",header = T)
+    overallScores_df_1 <<-read.csv("Data/1H/overall_score_raw_1H2017.csv",header = T)
     callScoresRank_df_1<<-read.csv("Data/1H/call_score_rank_1H2017.csv",header = T)
     dataScoresRank_df_1<<-read.csv("Data/1H/data_score_rank_1H2017.csv",header = T)
     smsScoresRank_df_1<<-read.csv("Data/1H/sms_score_rank_1H2017.csv",header = T)
     speedScoresRank_df_1<<-read.csv("Data/1H/speed_score_rank_1H2017.csv",header = T)
+    overallScoresRank_df_1 <<-read.csv("Data/1H/overall_score_rank_1H2017.csv",header = T)
   }
 }
 
@@ -32,20 +36,24 @@ pull_data_2017_2H<-function(star=TRUE,score=FALSE){
     dataStars_df_2<<-read.csv("Data/2H/data_star_raw_2H2017.csv",header = T)
     smsStars_df_2<<-read.csv("Data/2H/sms_star_raw_2H2017.csv",header = T)
     speedStars_df_2<<-read.csv("Data/2H/speed_star_raw_2H2017.csv",header = T)
+    overallStars_df_2 <<-read.csv("Data/2H/overall_star_raw_2H2017.csv",header = T)
     callStarsRank_df_2<<-read.csv("Data/2H/call_star_rank_2H2017.csv",header = T)
     dataStarsRank_df_2<<-read.csv("Data/2H/data_star_rank_2H2017.csv",header = T)
     smsStarsRank_df_2<<-read.csv("Data/2H/sms_star_rank_2H2017.csv",header = T)
     speedStarsRank_df_2<<-read.csv("Data/2H/speed_star_rank_2H2017.csv",header = T)
+    overallStarsRank_df_2 <<-read.csv("Data/2H/overall_star_rank_2H2017.csv",header = T)
   }
   if(score==TRUE){
     callScores_df_2<<-read.csv("Data/2H/call_score_raw_2H2017.csv",header = T)
     dataScores_df_2<<-read.csv("Data/2H/data_score_raw_2H2017.csv",header = T)
     smsScores_df_2<<-read.csv("Data/2H/sms_score_raw_2H2017.csv",header = T)
     speedScores_df_2<<-read.csv("Data/2H/speed_score_raw_2H2017.csv",header = T)
+    overallScores_df_2 <<-read.csv("Data/2H/overall_score_raw_2H2017.csv",header = T)
     callScoresRank_df_2<<-read.csv("Data/2H/call_score_rank_2H2017.csv",header = T)
     dataScoresRank_df_2<<-read.csv("Data/2H/data_score_rank_2H2017.csv",header = T)
     smsScoresRank_df_2<<-read.csv("Data/2H/sms_score_rank_2H2017.csv",header = T)
     speedScoresRank_df_2<<-read.csv("Data/2H/speed_score_rank_2H2017.csv",header = T)
+    overallScoresRank_df_2 <<-read.csv("Data/2H/overall_score_rank_2H2017.csv",header = T)
   }
 }
 
