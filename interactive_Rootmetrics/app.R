2313f653233484e5e4ba7b3aefa400417e2fe666
John Allen <john_allen@charleston.k12.sc.us>
2018-04-04 15:37:05 -0400

    app update: compare functionality in place

--- interactive_Rootmetrics/app.R
+++ interactive_Rootmetrics/app.R
@@ -2,6 +2,7 @@
 
 library(shiny)
 library(ggplot2)
+library(leaflet)
 source('../IndividualScoreScript.R')
 
 
@@ -9,51 +10,80 @@ source('../IndividualScoreScript.R')
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   # Application title
-  titlePanel("Data Histograms"
+  titlePanel("Rootmetrics RootStar Vs RootScore Analysis"
   ),
   
   sidebarLayout(
     sidebarPanel(tabsetPanel(
-      tabPanel('Plot Types',
+      
+      tabPanel('Individual',
+               textInput("userloc","Input User Location:",'66 George Street, Charleston, SC'),
+               selectInput('carrier','select which carrier',choices = c('AT&T','Sprint','T-Mobile','Verizon')),
+               numericInput('milesrad','Input a Radius to check:',value=5,min=0,max=100)),
+      tabPanel('Raw Distributions',
+               selectInput("carrier1",
+                           "Select Carrier 1:",
+                           choices = c('1 - AT.T','2 - Sprint','3 - T.Mobile','4 - Verizon','All')),
+               selectInput("carrier2",
+                           "Select Carrier 2:",
+                           choices = c('1 - AT.T','2 - Sprint','3 - T.Mobile','4 - Verizon','All')),
                selectInput("data_type",
                            "Select Data Type:",
                            choices = c('call','data','sms','speed')),
                selectInput("stars",
                            "Select Method:",
                            choices = c('star_raw','star_rank','score_raw','score_rank')),
-               
                selectInput("Year_Half",
                            "Select Time Period:",
+                           choices = c('1st Half','2nd Half'))
+               
+      ),
+      tabPanel('Comparisons',
+               selectInput("carrcomp1",
+                           "Carrier 1 to Explore",
+                           choices = c('1 - AT.T','2 - Sprint','3 - T.Mobile','4 - Verizon')),
+               selectInput("carrcomp2",
+                           "Carrier 2 to Explore",
+                           choices = c('1 - AT.T','2 - Sprint','3 - T.Mobile','4 - Verizon')),
+               selectInput("data_type1",
+                           "Select Data Type:",
+                           choices = c('call','data','sms','speed')),
+               selectInput("Year_Halfcomp1",
+                           "Select time period 1:",
                            choices = c('1st Half','2nd Half')),
-               selectInput("carrier1",
-                           "Select Carrier 1:",
-                           choices = c('1 - AT.T','2 - Sprint','3 - T.Mobile','4 - Verizon','All')),
-               selectInput("carrier2",
-                           "Select Carrier 2:",
-                           choices = c('1 - AT.T','2 - Sprint','3 - T.Mobile','4 - Verizon','All'))
-               ),
-      tabPanel('Comparisons',selectInput("comp1",
-                                          "select Comparison for carrier 1:",
-                                          choices = c('RootScore 1h-2h','RootStar 1h-2h','Rootscore 1h - RootStar 2h','Rootscore 1h - RootStar 1h','Rootscore 2h - RootStar 2h'))
+               selectInput("syscomp1",
+                           "select first system to see change from:",
+                           choices = c('RootScore','RootStar')),
+               selectInput("Year_Halfcomp2",
+                           "Select time period 2:",
+                           choices = c('2nd Half','1st Half')),
+               selectInput("syscomp2",
+                           "select first system to see change to:",
+                           choices = c('RootScore','RootStar'))
       )
-      ,
-      tabPanel('Individual',
-               textInput("userloc","Input User Location:",'66 George Street, Charleston, SC'),
-               selectInput('carrier','select which carrier',choices = c('AT&T','Sprint','T-Mobile','Verizon'))
-      )
-    )
       
+      
+    ),
+    submitButton(text = "Apply Changes", icon = NULL, width = NULL)
+    
     ),
     
     # Show a plot of the generated distribution
     
     mainPanel(tabsetPanel(
-      tabPanel('Raw Distributions', column(width =  8,plotOutput("distPlot",height = '100%'),plotOutput("distPlot1")),column(width=4,textOutput('mean1'),br(),textOutput('sd1'),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),textOutput('mean2'),br(),textOutput('sd2')))
-      ,tabPanel('Comparisons',leafletOutput('mapplot')),
-      submitButton(text = "Apply Changes", icon = NULL, width = NULL)
-      )
-              
-              )
+      tabPanel('individual Scores',leafletOutput('mapplot')),
+      tabPanel('Raw Distributions', 
+               column(width =  8,plotOutput("distPlot",height = '100%'),plotOutput("distPlot1")),column(width=4,textOutput('mean1'),br(),textOutput('sd1'),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),textOutput('mean2'),br(),textOutput('sd2')))
+      ,tabPanel('Comparisons',textOutput('titlecomp'),
+                tags$head(tags$style("#titlecomp{color: black;
+                                 font-size: 20px;
+                                     font-style: italic;
+                                   text-align: center;
+                                     }")),
+                br(),br(),br(),br(),plotOutput('comp11'))
+    )
+    
+    )
   )
 )
 
@@ -73,35 +103,7 @@ server <- function(input, output) {
     
   })
   
-  #data file for first portion comparison plot
-  filedata2 <- reactive({
-    if (is.null(input$stars)) {
-      # User has not uploaded a file yet
-      return(NULL)
-    }
-    if(input$comp1=='RootScore 1h-2h'){read.csv(paste('../Data/1H/',input$data_type,'_','score_rank','_1H2017.csv',sep = ''))}
-    else if(input$comp1=='RootStar 1h-2h'){read.csv(paste('../Data/1H/',input$data_type,'_','star_rank','_1H2017.csv',sep = ''))}
-    else if(input$comp1=='Rootscore 1h - RootStar 2h'){read.csv(paste('../Data/1H/',input$data_type,'_','score_rank','_1H2017.csv',sep = ''))}
-    else if(input$comp1=='Rootscore 1h - RootStar 1h'){read.csv(paste('../Data/1H/',input$data_type,'_','score_rank','_1H2017.csv',sep = ''))}
-    else if(input$comp1=='Rootscore 2h - RootStar 2h'){read.csv(paste('../Data/2H/',input$data_type,'_','score_rank','_1H2017.csv',sep = ''))}
-    
-    
-    })
-  
-  #data file for second portion comparison plot
-  filedata3 <- reactive({
-    if (is.null(input$stars)) {
-      # User has not uploaded a file yet
-      return(NULL)
-    }
-    if(input$comp1=='RootScore 1h-2h'){read.csv(paste('../Data/2H/',input$data_type,'_','score_rank','_1H2017.csv',sep = ''))}
-    else if(input$comp1=='RootStar 1h-2h'){read.csv(paste('../Data/2H/',input$data_type,'_','star_rank','_1H2017.csv',sep = ''))}
-    else if(input$comp1=='Rootscore 1h - RootStar 2h'){read.csv(paste('../Data/2H/',input$data_type,'_','star_rank','_1H2017.csv',sep = ''))}
-    else if(input$comp1=='Rootscore 1h - RootStar 1h'){read.csv(paste('../Data/1H/',input$data_type,'_','star_rank','_1H2017.csv',sep = ''))}
-    else if(input$comp1=='Rootscore 2h - RootStar 2h'){read.csv(paste('../Data/2H/',input$data_type,'_','star_rank','_1H2017.csv',sep = ''))}
-    
-    
-  })
+
   
   
   
@@ -302,80 +304,82 @@ server <- function(input, output) {
   
   
   
-  #comparison plot 1
-  output$comp11 <- renderPlot(height = 300,{
-    # draw the histogram with the specified number of bins
-    bins<-reactive({if(grepl('score_raw',input$stars)==T){1}
-      else if(grepl('star_raw',input$stars)==T){.25}
-      else if(grepl('rank',input$stars)==T){.5}})
-    
-    xmin.plot<-reactive({if(grepl('score_raw',input$stars)==T){70}
-      else if(grepl('star_raw',input$stars)==T){0}
-      else if(grepl('rank',input$stars)==T){0}})
-    
-    xmax.plot<-reactive({if(grepl('score_raw',input$stars)==T){100.5}
-      else if(grepl('star_raw',input$stars)==T){5.5}
-      else if(grepl('rank',input$stars)==T){4.5}})
-    
-    carr<-reactive({if(grepl('AT.T',input$carrier2)==T){2}
-      else if(grepl('Sprint',input$carrier2)==T){3}
-      else if(grepl('T.Mobile',input$carrier2)==T){4}
-      else if(grepl('Verizon',input$carrier2)==T){5}
-    })
-    
-    col.car<-reactive({if(grepl('AT.T',input$carrier2)==T){rgb(0,159,219,maxColorValue = 255)}
-      else if(grepl('Sprint',input$carrier2)==T){rgb(251,223,0,maxColorValue = 255)}
-      else if(grepl('T.Mobile',input$carrier2)==T){rgb(227,0,116,maxColorValue = 255)}
-      else if(grepl('Verizon',input$carrier2)==T){rgb(236,7,16,maxColorValue = 255)}
-    })
-    
-    
-    filedata1<-reactive({
-      
-      if(grepl('score_raw',input$stars)==T){
-        data.cur<-matrix(0,nrow = (xmax.plot()-xmin.plot())/bins()+1,ncol = 4)
-        
-        for(j in 1:4){
-          for(i in seq(xmin.plot(),xmax.plot(),bins())){
-            data.cur[(i-xmin.plot())/bins(),j]<-length(which(filedata()[,j+1]>=i & filedata()[,j+1]<(i+bins())))
-          }}
-        row.names(data.cur)<-seq(xmin.plot(),xmax.plot(),bins())
-      }
-      else{
-        data.cur<-matrix(0,nrow = length(seq(0,xmax.plot(),bins()*2)),ncol = 4)
-        for(j in 1:4){
-          for(i in seq(0,xmax.plot(),bins()*2)){
-            data.cur[i/(bins()*2)+1,j]<-length(which(filedata()[,j+1]==i))
-            
-          }}
-        row.names(data.cur)<-seq(xmin.plot(),xmax.plot(),bins()*2)
-      }
+  
+  
+  
+  #data file for first portion comparison plot 1
+  filedata2 <- reactive({
+    #build the file name
+    if(input$Year_Halfcomp1=='1st Half'){
+      read.csv(paste('../Data/1H/',input$data_type1,if(input$syscomp1=='RootScore'){'_score_rank'}else{'_star_rank'},'_1H2017.csv',sep = ''))
+    }else{read.csv(paste('../Data/2H/',input$data_type1,if(input$syscomp1=='RootScore'){'_score_rank'}else{'_star_rank'},'_2H2017.csv',sep = ''))
       
       
-      
-      
-      
-      colnames(data.cur)<-colnames(filedata())[2:5]
-      data.cur<-t(data.cur)
-      
-      return(data.cur)
-    })
+    }
     
-    
-    if(input$carrier2=='All'){barplot(filedata1(), main="All Carriers",
-                                      xlab="Score", beside=TRUE,col=carriers.col)}else{qplot(filedata()[,carr()],binwidth= bins(),col=I('black'),fill=I(col.car()), geom = 'histogram', main = paste(substr(input$carrier2,5,nchar(input$carrier2)),input$data_type,input$stars,input$Year_Half,sep = ' '),xlim = c(xmin.plot(),xmax.plot()),xlab = 'Score',ylab='Frequency')
-                                        
-                                      }
-  })
-  location.use<-reactive({if(input$userloc=='Enter address...'){'66 George Street, Charleston, SC'}else(input$userloc) })
-  carrier.use<-reactive({input$carrier})
-  plot.new<-reactive({  generate_individ_scores(targ_locat =input$userloc,carrier=input$carrier,radius_miles = 5,in_app=T)
   })
   
-  output$mapplot<-renderLeaflet({
-    plot.new()
-  #generate_individ_scores(targ_locat = location.use(),carrier=carrier.use(),radius_miles = 5,in_app=T)
+  #data file for first portion comparison plot 2
+  filedata3 <- reactive({
+    #build the file name
+    if(input$Year_Halfcomp2=='1st Half'){
+      read.csv(paste('../Data/1H/',input$data_type1,if(input$syscomp2=='RootScore'){'_score_rank'}else{'_star_rank'},'_1H2017.csv',sep = ''))
+    }else{read.csv(paste('../Data/2H/',input$data_type1,if(input$syscomp2=='RootScore'){'_score_rank'}else{'_star_rank'},'_2H2017.csv',sep = ''))
+      
+      
+    }
+    
+  })
+  
+  #comparison plot 1
+  output$comp11 <- renderPlot(height = 300,{
+
+    col.car1<-reactive({if(grepl('AT.T',input$carrcomp1)==T){rgb(0,159,219,maxColorValue = 255)}
+      else if(grepl('Sprint',input$carrcomp1)==T){rgb(251,223,0,maxColorValue = 255)}
+      else if(grepl('T.Mobile',input$carrcomp1)==T){rgb(227,0,116,maxColorValue = 255)}
+      else if(grepl('Verizon',input$carrcomp1)==T){rgb(236,7,16,maxColorValue = 255)}
     })
+    
+    col.car2<-reactive({if(grepl('AT.T',input$carrcomp2)==T){rgb(0,159,219,maxColorValue = 255)}
+      else if(grepl('Sprint',input$carrcomp2)==T){rgb(251,223,0,maxColorValue = 255)}
+      else if(grepl('T.Mobile',input$carrcomp2)==T){rgb(227,0,116,maxColorValue = 255)}
+      else if(grepl('Verizon',input$carrcomp2)==T){rgb(236,7,16,maxColorValue = 255)}
+    })
+    
+    
+    carr1<-reactive({if(grepl('AT.T',input$carrcomp1)==T){2}
+      else if(grepl('Sprint',input$carrcomp1)==T){3}
+      else if(grepl('T.Mobile',input$carrcomp1)==T){4}
+      else if(grepl('Verizon',input$carrcomp1)==T){5}
+    })
+    carr2<-reactive({if(grepl('AT.T',input$carrcomp2)==T){2}
+      else if(grepl('Sprint',input$carrcomp2)==T){3}
+      else if(grepl('T.Mobile',input$carrcomp2)==T){4}
+      else if(grepl('Verizon',input$carrcomp2)==T){5}
+    })
+    
+    
+    
+    DF <- rbind(data.frame(fill=col.car1(), obs=filedata3()[,carr1()]-filedata2()[,carr1()]),
+                data.frame(fill=col.car2(), obs=filedata3()[,carr2()]-filedata2()[,carr2()]))
+    
+    ggplot(DF, aes(x=obs, fill=fill)) +
+      geom_histogram(binwidth = 1 ,colour="black", position="dodge") +
+      scale_fill_identity()+
+      labs(title(main = paste('Comparing',input$data_type1,'rankings of',input$carrcomp1,'and',input$carrcomp2,'change from',input$Year_Halfcomp1,input$syscomp1, 'to',input$Year_Halfcomp2,input$syscomp2)),
+           x='Rank Change',y='Frequency')
+    
+    
+  })
+  
+  output$titlecomp<-renderText({paste('Comparing',input$data_type1,'rankings of',substr(input$carrcomp1,5,nchar(input$carrcomp1)),'and',substr(input$carrcomp2,5,nchar(input$carrcomp2)),'change from',input$Year_Halfcomp1,input$syscomp1, 'to',input$Year_Halfcomp2,input$syscomp2)})
+  
+  output$mapplot<-renderLeaflet({
+    generate_individ_scores(targ_locat =input$userloc,carrier=input$carrier,radius_miles = input$milesrad,in_app=T)
+    })
+  
+  
+  
 
   
 }
